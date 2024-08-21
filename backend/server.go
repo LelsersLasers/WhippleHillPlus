5138399882b64bd0e560aacc37cfacbffc37cd63
Lord-Lelsers <millankumar@gmail.com>
2024-08-21 17:04:02 -0400

    Simple HTTP server

--- backend/server.go
+++ backend/server.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+
+const Port = 8080
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	fmt.Printf("Server is running on port %d\n", Port)
+
+	addr := fmt.Sprintf(":%d", Port)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		fmt.Println("Error starting server: ", err)
+	}
 }
