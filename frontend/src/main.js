805c3a1564fe88ef76c52881354ff2456f7b3f65
Lord-Lelsers <millankumar@gmail.com>
2024-08-25 15:40:53 -0400

    Moving run.sh up a folder

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import App from './App.svelte';
 
-const API = "http://64.98.192.13:3001";
+const API = "http://64.98.192.13:3003";
+// const API = "http://localhost:3003";
 const app = new App({
 	target: document.body,
 	props: {
