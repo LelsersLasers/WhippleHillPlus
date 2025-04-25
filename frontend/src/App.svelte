6430127fd8e7ac302cc2ba46184c3e6ec045ff5e
Millan Kumar <millankumar@gmail.com>
2025-04-25 17:43:49 -0400

    Maybe improving error redirect?

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -29,7 +29,7 @@
             .then((data) => {
                 console.log(data);
 
-                if (data["error"]) {
+                if (data["error"] && data["error"] != "") {
                     window.location.href = "/login";
                     return;
                 }
